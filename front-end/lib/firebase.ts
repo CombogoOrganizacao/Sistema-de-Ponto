3e605c1272572b23838ed2aeefcddaf347b6bf8b
Combogo <rennancr93@gmail.com>
2026-08-26 14:34:12 -0300

    firebase fix

--- front-end/lib/firebase.ts
+++ front-end/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3Tjefzcy-YxOr3c8YJ91HhH8AO3r1LuY",
-  authDomain: "combogoponto.firebaseapp.com",
-  projectId: "combogoponto",
-  storageBucket: "combogoponto.firebasestorage.app",
-  messagingSenderId: "20657185811",
-  appId: "1:20657185811:web:21b6111d7a29b370a20be6",
-  measurementId: "G-BJZKEBN9FH"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyD3Tjefzcy-YxOr3c8YJ91HhH8AO3r1LuY",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "combogoponto.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "combogoponto",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "combogoponto.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "20657185811",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:20657185811:web:21b6111d7a29b370a20be6",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-BJZKEBN9FH"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
